3fb80d4fc77fac4f7fbb97fa1adf185456709f24
Ruslan <Giovingo92@gmail.com>
2024-10-21 15:18:14 +0300

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const num1 = '10'
+const num2 = 10
+
+const num3 = 10
+const num4 = 5
+
+// console.log('check', num1 ==== num2) // Error
+console.log('check', num1 > num2)
+console.log('check', num3 > num4)
