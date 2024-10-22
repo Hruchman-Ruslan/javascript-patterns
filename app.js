988a3c2c8423762f8df9b399baace9eb7bc1fe68
Ruslan <Giovingo92@gmail.com>
2024-10-22 19:09:11 +0300

    feat: class construction

--- app.js
+++ app.js
@@ -2,16 +2,38 @@
 
 // function construction
 
-function User(name, age, country) {
-	this.name = name
-	this.age = age
-	this.country = country
+// function User(name, age, country) {
+// 	this.name = name
+// 	this.age = age
+// 	this.country = country
 
-	this.setNewAge = function (newAge) {
+// 	this.setNewAge = function (newAge) {
+// 		this.age = newAge
+// 	}
+
+// 	this.showAge = function () {
+// 		console.log('The new age is ' + this.age)
+// 	}
+// }
+
+// const firstObject = new User('Ruslan', 31, 'Ukraine')
+// firstObject.setNewAge(32)
+// firstObject.showAge()
+
+// class construction
+
+class User {
+	construction(name, age, country) {
+		this.name = name
+		this.age = age
+		this.country = country
+	}
+
+	setNewAge(newAge) {
 		this.age = newAge
 	}
 
-	this.showAge = function () {
+	showAge() {
 		console.log('The new age is ' + this.age)
 	}
 }
