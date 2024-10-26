6509bb88c18c518077bb20195993e672239b1461
Ruslan <Giovingo92@gmail.com>
2024-10-26 13:24:26 +0300

    feat: scope

--- app.js
+++ app.js
@@ -1,52 +1,14 @@
 'use strict'
 
-function outerFunction(outerVariable) {
-	function innerFunction(innerVariable) {
-		console.log('outerVariable', outerVariable)
-		console.log('innerValue', innerVariable)
+const globalScope = 'value'
+function funcScope(someArg) {
+	let value = 'test'
+	console.log('func test', test)
+	if (someArg) {
+		const test = 1
+		value = globalScope
 	}
-
-	return innerFunction
+	return value
 }
 
-const closure = outerFunction('outerValue')
-
-closure('innerValue')
-
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||| currying
-
-function firstFunc() {
-	return function () {
-		return 'second function'
-	}
-}
-
-const result = firstFunc()
-
-console.log('result', result)
-
-console.log(result())
-
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||| memoization
-
-function getVideoInfo(videoId) {
-	const cache = {}
-
-	return function (videoId) {
-		if (!cache[videoId]) {
-			console.log('Requesting server for video information with ID:', videoId)
-			cache[videoId] = {
-				title: 'Video Title',
-				duration: 'Video Duration',
-				author: 'Video Author',
-			}
-		}
-		return cache[videoId]
-	}
-}
-
-const memoizedGetVideoInfo = getVideoInfo()
-
-console.log(memoizedGetVideoInfo(123))
-
-console.log(memoizedGetVideoInfo(123))
+funcScope()
