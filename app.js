1fc194406d90425aba686baef930e9c15547328f
Ruslan <Giovingo92@gmail.com>
2024-10-26 13:34:09 +0300

    feat: adding description

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 'use strict'
 
-const globalScope = 'value'
+// Global scope
+const globalScope = 'value' // global variable, accessible throughout the code
+
 function funcScope(someArg) {
-	let value = 'test'
-	console.log('func test', test)
+	// Function scope (funcScope)
+	let value = 'test' // variable, visible only inside funcScope
+	console.log('func test', test) // this will throw an error because test is not declared in this block yet
+
 	if (someArg) {
-		const test = 1
-		value = globalScope
+		// Block scope (if block)
+		const test = 1 // variable, visible only inside this if block
+		value = globalScope // changing the value of 'value' to the value of the global variable globalScope
 	}
-	return value
+
+	return value // returns the value of 'value' defined in the function scope
 }
 
-funcScope()
+funcScope() // calling the function
