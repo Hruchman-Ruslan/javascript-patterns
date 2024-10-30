ad4d0d047899f3d0add84acfdd6ce801de8c6375
Ruslan <Giovingo92@gmail.com>
2024-10-30 14:44:24 +0200

    feat: strategy

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
-// Class representing a collection
-class IterableCollection {
-	constructor() {
-		this.collection = []
+// A class that uses a strategy
+class Context {
+	constructor(strategy) {
+		this.strategy = strategy
 	}
 
-	// Adding an item to the collection
-	addItem(item) {
-		this.collection.push(item)
+	// A method that establishes a new strategy
+	setStrategy(strategy) {
+		this.strategy = strategy
 	}
 
-	// Returning an iterator for the collection
-	getIterator() {
-		return new CollectionIterator(this.collection)
+	// A method that performs an action according to a strategy
+	executedStrategy() {
+		return this.strategy.execute()
 	}
 }
 
-// Iterator class
-class CollectionIterator {
-	constructor(collection) {
-		this.collection = collection
-		this.index = 0
-	}
+// Interface strategy
+class Strategy {
+	execute() {}
+}
 
-	// Method that checks if there are still elements to iterate
-	hasNext() {
-		return this.index < this.collection.length
+// Concrete strategy №1
+class ConcreteStrategy1 extends Strategy {
+	execute() {
+		console.log('Executing strategy 1')
 	}
+}
 
-	// Method that returns the next element
-	next() {
-		return this.collection[this.index++]
+// Concrete strategy №2
+class ConcreteStrategy2 extends Strategy {
+	execute() {
+		console.log('Executing strategy 2')
 	}
 }
 
 // Usage:
-const collection = new IterableCollection()
-collection.addItem('Item 1')
-collection.addItem('Item 2')
-collection.addItem('Item 3')
 
-const iterator = collection.getIterator()
-while (iterator.hasNext()) {
-	console.log(iterator.next())
-}
+const context = new Context(new ConcreteStrategy1())
+console.log(context.executedStrategy()) // Usage strategy №1
+
+context.setStrategy(new ConcreteStrategy2())
+console.log(context.executedStrategy()) // Usage strategy №2
